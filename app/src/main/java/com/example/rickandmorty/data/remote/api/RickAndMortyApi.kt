d40043968636efca9242a9f8a42947c4b6416dcb
saraalves <saraalves14@hotmail.com>
2023-06-28 21:02:16 -0300

    adicionando gradle properties e service

---
+++ app/src/main/java/com/example/rickandmorty/data/remote/api/RickAndMortyApi.kt
@@ -0,0 +1,10 @@
+package com.example.rickandmorty.data.remote.api
+
+//import com.picpay.desafio.android.data.remote.model.UserResponse
+//import retrofit2.http.GET
+
+//interface RickAndMortyApi {
+//
+//    @GET("users")
+//    suspend fun getUsers(): List<UserResponse>
+//}

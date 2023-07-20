2baf89da3b16242995f17cad3fc2e3d1370b2d65
saraalves <saraalves14@hotmail.com>
2023-07-20 14:46:07 -0300

    criando navigation bar

--- app/src/main/java/com/example/rickandmorty/RickAndMortyActivity.kt
+++ app/src/main/java/com/example/rickandmorty/RickAndMortyActivity.kt
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.saraalves.rickandmorty.R
 
-class RickAndMortyActivity : AppCompatActivity() {
+class RickAndMortyActivity : AppCompatActivity(R.layout.activity_rick_and_morty) {
+
+    private val binding by lazy {
+        RickAndMortyActivityBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rick_and_morty)
